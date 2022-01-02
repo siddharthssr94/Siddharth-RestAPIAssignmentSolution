5a93965364df26894da18e272f1a1bffb8b5d658
siddharthssr94 <87899676+siddharthssr94@users.noreply.github.com>
2022-01-02 21:21:12 +0530

    Delete EmployeeManagementSystem directory

--- EmployeeManagementSystem/src/main/java/com/greatlearning/ems/exception/ResouceInvalidException.java
+++
@@ -1,8 +0,0 @@
-package com.greatlearning.ems.exception;
-
-public class ResouceInvalidException extends RuntimeException {
-
-	public ResouceInvalidException(Class resourse, String conflictMsg) {
-		super(String.format("%1$s details are not valid : %2$s", resourse.getSimpleName(), conflictMsg));
-	}
-}
--- EmployeeManagementSystem/src/main/java/com/greatlearning/ems/exception/ResouceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.greatlearning.ems.exception;
-
-public class ResouceNotFoundException extends RuntimeException {
-
-	public ResouceNotFoundException(Class resourse, Long id) {
-		super(String.format("Could not find %1$s %2$s", resourse.getSimpleName(), id));
-	}
-}
